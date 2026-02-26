3bc6ec9de225a0aed016323de03e88f62c7a0482
clarissa-dev-codes <clarissaedwards99@gmail.com>
2026-02-26 00:14:22 -0600

    Add files via upload
    - Updated Main to host all eggs
    - Legendary egg added
    - Rare egg added
    - Common egg added

--- Startols/main.py
+++ Startols/main.py
@@ -33,15 +33,18 @@ season_height = 100
 
 seasonxpos = HEIGHT//1
 seasonypos = WIDTH//2.35
-commonxpos = HEIGHT//9.94
+commonxpos = HEIGHT//11
 commonypos = WIDTH//2.35
-rarexpos = HEIGHT//3
+rarexpos = HEIGHT//4
 rareypos = WIDTH//5
 legendxpos = HEIGHT//1.2
 legendypos = WIDTH//5
 
 try:
     season_image = pygame.image.load("SeasonalEgg.png").convert_alpha()
+    common_image = pygame.image.load("CommonEgg.png").convert_alpha()
+    rare_image = pygame.image.load("RareEgg.png").convert_alpha()
+    legend_image = pygame.image.load("LegendaryEgg.png").convert_alpha()
 except pygame.error as message:
     print(f"Cannot load image: SeasonalEgg.png - {message}")
 
@@ -49,11 +52,17 @@ season_rect = season_image.get_rect()
 scaled_season_egg = pygame.transform.scale(season_image, (season_width, season_height))
 season_egg_rect = scaled_season_egg.get_rect(topleft=(seasonxpos, seasonypos))
 
-#season_rect = pygame.Rect(seasonxpos, seasonypos, season_width, season_height)
-common_rect = pygame.Rect(commonxpos, commonypos, season_width, season_height)
-rare_rect = pygame.Rect(rarexpos, rareypos, season_width, season_height)
-legend_rect = pygame.Rect(legendxpos, legendypos, season_width, season_height)
+common_rect = common_image.get_rect()
+scaled_common_egg = pygame.transform.scale(common_image, (season_width, season_height))
+common_egg_rect = scaled_common_egg.get_rect(topleft=(commonxpos, commonypos))
 
+rare_rect = rare_image.get_rect()
+scaled_rare_egg = pygame.transform.scale(rare_image, (season_width, season_height))
+rare_egg_rect = scaled_rare_egg.get_rect(topleft=(rarexpos, rareypos))
+
+legend_rect = legend_image.get_rect()
+scaled_legend_egg = pygame.transform.scale(legend_image, (season_width, season_height))
+legend_egg_rect = scaled_legend_egg.get_rect(topleft=(legendxpos, legendypos))
 
 amp = 20
 frequency = 0.002
@@ -68,16 +77,15 @@ while running:
 
     vertical_off = amp * math.sin(time * frequency)
     season_egg_rect.y = int(seasonypos + (vertical_off//-0.5))
-    common_rect.y = int(commonypos + (vertical_off//-0.9))
-    rare_rect.y = int(rareypos + (vertical_off//0.7))
-    legend_rect.y = int(legendypos + vertical_off)
+    common_egg_rect.y = int(commonypos + (vertical_off//-0.9))
+    rare_egg_rect.y = int(rareypos + (vertical_off//0.7))
+    legend_egg_rect.y = int(legendypos + vertical_off)
 
     window.blit(bg_image, (0,0))
     window.blit(scaled_season_egg, season_egg_rect)
-
-    pygame.draw.rect(window, WHITE, common_rect)
-    pygame.draw.rect(window, WHITE, rare_rect)
-    pygame.draw.rect(window, WHITE, legend_rect)
+    window.blit(scaled_common_egg, common_egg_rect)
+    window.blit(scaled_rare_egg, rare_egg_rect)
+    window.blit(scaled_legend_egg, legend_egg_rect)
 
     pygame.display.flip()
 
